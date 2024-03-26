39a74a4c751aa4d1e85f40805f476520b5df1606
NadezhdaT <kybalibri@mail.ru>
2024-03-26 16:05:36 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 2105;
+        int percent = 20;
+
+        int bonus = amount / percent;
+
+        System.out.println("Бонусные мили" + bonus);
+
+    }
+}
